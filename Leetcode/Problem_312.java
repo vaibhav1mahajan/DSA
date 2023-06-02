a350d129e3aa883f612e2777aebf19e7fed34f43
vaibhav1mahajan <vaibhavmahajan2257@gmail.com>
2023-06-02 17:18:06 +0530

    solution of problem number 312 of leetcode

---
+++ Leetcode/Problem_312.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minInsertions(String x) {
+
+        StringBuilder dummy = new StringBuilder(x);
+        dummy = dummy.reverse();
+        String y = dummy.toString();
+        int n = x.length();
+        int m = y.length();
+        int[][] ans = new int[n + 1][m + 1];
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (x.charAt(i - 1) == y.charAt(j - 1)) {
+                    ans[i][j] = 1 + ans[i - 1][j - 1];
+                } else {
+                    ans[i][j] = Math.max(ans[i][j - 1], ans[i - 1][j]);
+                }
+            }
+        }
+        return /* Deletion */(n - ans[n][m]);
+    }
+}
